88732c99368790e583972bc9c8b2ba52c0533455
jpateros <jpateros5410@sdsu.edu>
2024-10-12 14:31:29 -0700

    triangle done

---
+++ beginning/js/05-triangle.js
@@ -0,0 +1,8 @@
+// Write a loop that displays the following triangle within a console window:
+
+var triangle = "";
+
+for (var i = 1; i <= 7; i++) {
+    triangle += "#"; 
+    console.log(triangle); 
+}
